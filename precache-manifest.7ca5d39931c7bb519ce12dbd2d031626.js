2300b31771ce624d3a29b17592a5cd6305106031
ling <l13789060033@163.com>
2022-03-11 17:15:06 +0800

    deploy

---
+++ precache-manifest.7ca5d39931c7bb519ce12dbd2d031626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa4e4ac9c1db57e23e6",
+    "url": "/test/css/app.22c78abd.css"
+  },
+  {
+    "revision": "ac862c8e502e364df1850133b4d4c995",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "aaa4e4ac9c1db57e23e6",
+    "url": "/test/js/app.862e23f6.js"
+  },
+  {
+    "revision": "b9f878487d47994f0b2e",
+    "url": "/test/js/chunk-vendors.207e0939.js"
+  },
+  {
+    "revision": "75e307defacad44e68a2e2f02dda503d",
+    "url": "/test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test/robots.txt"
+  }
+]);
